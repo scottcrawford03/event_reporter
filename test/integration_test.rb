cc79aca9bfd0a70f641a24a1c92d86b316cf35d2
Rich Shea <richcshea@gmail.com>
2014-11-15 11:55:15 -0700

    Search by last name successfully creates new array of objects that all of the reporter methods can be called on.

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -1,12 +1,13 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative '../lib/command_center'
+require_relative '../lib/event_manager'
 
 class IntegrationTest < Minitest::Test
   def test_it_lookup_by_last_name
-    command_center = CommandCenter.new
-    entries = command_center.look_up('Parker').sort_by { |name| name.first_name }
+    reporters = []
+    event_manager = EventManager.new(reporters)
+    entries = event_manager.look_up('Parker').sort_by { |name| name.first_name }
 
     assert_equal 3, entries.size
     e1, e2, e3 = entries
