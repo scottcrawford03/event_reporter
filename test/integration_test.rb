0ac6ec9eb0e3536adce83828b0a09e5e53071bdb
Scott Crawford <scottcrawford03@gmail.com>
2014-11-15 10:28:13 -0700

    got reporter_test passing with initial reporter class

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -11,13 +11,17 @@ class IntegrationTest < Minitest::Test
     assert_equal 3, entries.size
     e1, e2, e3 = entries
 
-    assert_equal "Agnes Parker", e1.name
-    assert_equal "758.942.6890", e1.phone_number
+    assert_equal "Gregory Parker", e1.name
+    assert_equal "718-305-4000", e1.phone_number
 
-    assert_equal "Craig Parker", e2.name
-    assert_equal "716-133-3210", e2.phone_number
+    assert_equal "Patrick Parker", e2.name
+    assert_equal "718-305-4000", e2.phone_number
 
-    assert_equal "Mohamed Parker", e3.name
-    assert_equal "701-655-6889", e3.phone_number
+    assert_equal "Scott Parker", e3.name
+    assert_equal "202-270-1000", e3.phone_number
   end
 end
+
+# 3949,1/31/09 23:00,Gregory,Parker,jhopenh1@jumpstartlab.com,718-305-4000,"7123 Penn Avenue, #3",Pittsburgh,PA,15208
+# 4236,2/1/09 17:05,Patrick,Parker,qkika_farrell@jumpstartlab.com,718-305-4000,4509 Frankford Avenue,Philadelphia,PA,19214
+# 4551,2/1/09 20:53,Scott,Parker,qhuer321@jumpstartlab.com,202-270-1000,19308 Alderbarn Ct,Brookeville,MD,20833
