b75a907a4482eb74cf4fe42653e251b6b15d428d
Scott Crawford <scottcrawford03@gmail.com>
2014-11-19 13:41:28 -0700

    tables are working and updating

--- lib/table_maker.rb
+++ lib/table_maker.rb
@@ -11,12 +11,21 @@ class TableMaker
 
 
   def create_table
-  Terminal::Table.new headings: ['Last Name', 'First Name', 'Email', 'Home Phone', 'City', 'State', 'Zipcode'],
+  Terminal::Table.new headings: ['Last Name', 'First Name', 'Email', 'Home Phone', 'Street', 'City', 'State', 'Zipcode'],
                       rows: rows
   end
 
-  def update(attendee_traits)
-      attendee_traits.each_index { |index| rows << attendee_traits[index] }
+  def update(new_queue)
+      new_queue.each { |attendee| rows << ["#{attendee.last_name.capitalize}",
+                                           "#{attendee.first_name.capitalize}",
+                                           "#{attendee.email}",
+                                           "#{attendee.home_phone}",
+                                           "#{attendee.street}",
+                                           "#{attendee.city.capitalize}",
+                                           "#{attendee.state.upcase}",
+                                           "#{attendee.zipcode}"             ] }
+    @table = create_table
+
   end
 
   def show
